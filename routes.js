800d578e3781aa733b2b88d8153ab0644cd385f5
ACC <64018848+Heliandro1@users.noreply.github.com>
2023-02-03 10:44:50 +0100

    creating the homepage

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
+
 route.get('/', homeController.homePage);
-route.post('/', homeController.post);
+
 module.exports = route;
