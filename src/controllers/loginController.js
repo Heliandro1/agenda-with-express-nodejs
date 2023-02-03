474392368653559bafba902dff550ef0c6eae972
ACC <64018848+Heliandro1@users.noreply.github.com>
2023-02-03 11:57:01 +0100

    adding the validator package

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -3,7 +3,15 @@ exports.index = (req, res) =>{
     res.render('login');
 }
 
-exports.register = (req, res) =>{
+exports.register = async (req, res) =>{
     const login = new Login(req.body);
-    res.send(login.body);
+    await login.register();
+    if(login.errors.length > 0){
+        req.flash('errors', login.errors);
+        req.session.save(()=>{
+            return res.redirect('back');
+        });
+        return;
+    }
+    res.send(login.errors);
 }
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-
+const validator = require('validator');
 const LoginSchema = new mongoose.Schema({
-    titulo: {type: String, require: true},
-    descricao: String
-
+    email: {type: String, required: true},
+    password: {type: String, required: true}
 });
 const LoginModel = mongoose.model('Login', LoginSchema);
 class Login{
@@ -12,9 +11,38 @@ class Login{
         this.errors = [];
         this.users = null;
     }
+    async register(){
+        this.valida();
+        if(this.errors.length > 0) return;
+        try {
+            this.user = await LoginModel.create(this.body);
+        } catch (err) {
+            console.log(err);
+        }
+        
+    }
 
     valida(){
-        
+        this.cleanUp();
+        if(!validator.default.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+
+        if(this.body.password < 3 || this.body.password > 50){
+            this.errors.push('A senha precisa ter entre 3 e 50 caracteres');
+        }
+    }
+    cleanUp(){
+        for (const key in this.body) {
+            if (Object.hasOwnProperty.call(this.body, key)) {
+                const element = this.body[key];
+                if (typeof element !== 'string') {
+                    this.body[key] = '';
+                }
+            }
+        }
+        this.body = {
+            email: this.body.email,
+            password: this.body.password
+        }
     }
 }
 module.exports = Login;
